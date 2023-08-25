eed02620041a976112bdd3748c191e3e2973b1a7
Joao Pedro Da Silva Kriger <23108522@facin.portoalegre.pucrsnet.br>
2023-08-25 18:08:01 -0300

    git commit

--- java/src/Cliente.java
+++ java/src/Cliente.java
@@ -1,3 +1,51 @@
 public class Cliente{
-    
+    private int cpf;
+    private String nome;
+    private int idade;
+    private String genero;
+
+    public Cliente(int idade,int cpf,String nome,String genero){
+        this.setGenero(genero);
+        this.setCpf(cpf);
+        this.setNome(nome);
+        this.setIdade(idade);
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+
+
+
+
+
 }
